d9de67255b88023a5233b7668091d4e016ac5059
LuigiAnaclet <124635849+LuigiAnaclet@users.noreply.github.com>
2025-08-07 13:11:36 -0400

    Update page.tsx

--- app/admin/menu/page.tsx
+++ app/admin/menu/page.tsx
@@ -65,7 +65,7 @@ export default function AdminMenuPage() {
     await fetch('/api/menu', {
       method: 'DELETE',
       headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ id, password })
+      body: JSON.stringify({ id })
     });
     fetchMenu();
   };
