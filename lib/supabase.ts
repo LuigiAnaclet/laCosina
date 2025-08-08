9b9e7454a25efe0ffea9865545cb84997d991e95
LuigiAnaclet <124635849+LuigiAnaclet@users.noreply.github.com>
2025-08-08 12:02:03 -0400

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,7 +1,8 @@
-// ✅ pour le client (pages, composants interactifs)
+
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
+
