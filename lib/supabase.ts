db156a30b1ce317077336cb778d188adc5f9f7ad
LuigiAnaclet <124635849+LuigiAnaclet@users.noreply.github.com>
2025-08-08 10:52:26 -0400

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,17 +1,7 @@
+// ✅ pour le client (pages, composants interactifs)
 import { createClient } from '@supabase/supabase-js';
-import { cookies } from 'next/headers';
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
-export const supabase = createClient(
-  supabaseUrl,
-  supabaseKey,
-  process.env.SUPABASE_SERVICE_ROLE_KEY!,
-  {
-    cookies: {
-      get: (key) => cookies().get(key)?.value,
-    },
-  });
-
-
+export const supabase = createClient(supabaseUrl, supabaseKey);
