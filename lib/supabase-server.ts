89e23b68477ff247a03b4f7e819c92523fbe03d8
LuigiAnaclet <124635849+LuigiAnaclet@users.noreply.github.com>
2025-08-08 10:53:04 -0400

    Create supabase-server.ts

---
+++ lib/supabase-server.ts
@@ -0,0 +1,19 @@
+import { cookies } from 'next/headers'
+import { createServerClient } from '@supabase/auth-helpers-nextjs'
+import { type SupabaseClient } from '@supabase/supabase-js'
+
+export function createSupabaseServerClient(): SupabaseClient {
+  const cookieStore = cookies()
+
+  return createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        get(name: string) {
+          return cookieStore.get(name)?.value
+        },
+      },
+    }
+  )
+}
